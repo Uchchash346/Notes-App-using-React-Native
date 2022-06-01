9f8f22b95c843a7d7b10ed3496dd0d3d50454fa9
Uchchash346 <mru416@gmail.com>
2022-06-01 11:11:40 +0600

    Project Initialization

--- App.js
+++ App.js
@@ -2,11 +2,25 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/screens/home';
+import SignIn from './src/screens/signin';
+import SignUp from './src/screens/signup';
+import Create from './src/screens/create';
+import Edit from './src/screens/edit';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-     
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="Create" component={Create} />
+        <Stack.Screen name="Edit" component={Edit} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
