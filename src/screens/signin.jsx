d0622d6bdf21348f8f2ef00cffe1ba1c63af1945
Uchchash346 <mru416@gmail.com>
2022-06-21 15:39:38 +0600

    Login page completed

--- src/screens/signin.jsx
+++ src/screens/signin.jsx
@@ -1,17 +1,25 @@
-import { View, SafeAreaView, Text, Image, TextInput, StyleSheet } from 'react-native'
+import { View, SafeAreaView, Text, Image, TextInput, StyleSheet, Pressable } from 'react-native'
 import React from 'react'
+import Button from '../components/Button'
 
 export default function SignIn() {
     return (
-        <SafeAreaView>
+        <SafeAreaView style={{ flex: 1 }}>
             <Image source={require('../../assets/login-image.png')}
                 style={{ alignSelf: 'center' }}
             />
             <Text style={{ fontSize: 18, fontWeight: 'bold', textAlign: 'center' }}>
                 Never forget your notes
             </Text>
-            <View>
-                <TextInput placeholder="Email" style={styles.input} />
+            <View style={{ paddingHorizontal: 16, paddingVertical: 25 }}>
+                <TextInput placeholder="Email Address" style={styles.input} />
+                <TextInput placeholder="Password" style={styles.input} secureTextEntry />
+            </View>
+            <View style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'center' }}>
+                <Button title={"Login"} customStyles={{ alignSelf: 'center', marginBottom: 60 }} />
+                <Pressable>
+                    <Text>Don't have an account?{" "}<Text style={{ color: 'green', fontWeight: 'bold' }}>Sign Up</Text></Text>
+                </Pressable>
             </View>
         </SafeAreaView>
     )
@@ -20,6 +28,8 @@ export default function SignIn() {
 const styles = StyleSheet.create({
     input: {
         height: 48,
-        borderWidth: 1,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc',
+        marginBottom: 25,
     }
 })
