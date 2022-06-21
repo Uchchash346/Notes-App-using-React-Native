9a5f1f641c45c4ab8a37b8f8991998415a6dbc64
Uchchash346 <mru416@gmail.com>
2022-06-21 16:00:43 +0600

    Edit Login page and setup sign up page

--- src/screens/signin.jsx
+++ src/screens/signin.jsx
@@ -1,8 +1,9 @@
 import { View, SafeAreaView, Text, Image, TextInput, StyleSheet, Pressable } from 'react-native'
 import React from 'react'
-import Button from '../components/Button'
+import Button from '../components/Button';
+import Input from '../components/Input'
 
-export default function SignIn() {
+export default function SignIn({ navigation }) {
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <Image source={require('../../assets/login-image.png')}
@@ -12,24 +13,15 @@ export default function SignIn() {
                 Never forget your notes
             </Text>
             <View style={{ paddingHorizontal: 16, paddingVertical: 25 }}>
-                <TextInput placeholder="Email Address" style={styles.input} />
-                <TextInput placeholder="Password" style={styles.input} secureTextEntry />
+                <Input placeholder="Email Address" />
+                <Input placeholder="Password" secureTextEntry />
             </View>
             <View style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'center' }}>
                 <Button title={"Login"} customStyles={{ alignSelf: 'center', marginBottom: 60 }} />
-                <Pressable>
+                <Pressable onPress={() => { navigation.navigate('SignUp') }}>
                     <Text>Don't have an account?{" "}<Text style={{ color: 'green', fontWeight: 'bold' }}>Sign Up</Text></Text>
                 </Pressable>
             </View>
         </SafeAreaView>
     )
 }
-
-const styles = StyleSheet.create({
-    input: {
-        height: 48,
-        borderBottomWidth: 1,
-        borderBottomColor: '#ccc',
-        marginBottom: 25,
-    }
-})
--- src/screens/signup.jsx
+++ src/screens/signup.jsx
@@ -1,10 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, SafeAreaView, Text, Image, TextInput, StyleSheet, Pressable } from 'react-native'
 import React from 'react'
+import Button from '../components/Button'
+import Input from '../components/Input'
 
 export default function SignUp() {
     return (
-        <View>
-            <Text>SignUp</Text>
-        </View>
+        <SafeAreaView style={{ flex: 1 }}>
+            <View style={{ paddingHorizontal: 16, paddingVertical: 25 }}>
+                <Input placeholder="Email Address" />
+                <Input placeholder="Full Name" />
+                <Input placeholder="Age" />
+                <Input placeholder="Password" secureTextEntry />
+            </View>
+            <View style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'center' }}>
+                <Button title={"Sign Up"} customStyles={{ alignSelf: 'center', marginBottom: 60 }} />
+                <Pressable>
+                    <Text>Already have an account?{" "}
+                        <Text style={{ color: 'green', fontWeight: 'bold' }}>Sign In</Text>
+                    </Text>
+                </Pressable>
+            </View>
+        </SafeAreaView>
     )
 }
